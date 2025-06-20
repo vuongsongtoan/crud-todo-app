90dff1f6f4572e527faceca662b2996c864d4578
vuongsongtoan <130550092+vuongsongtoan@users.noreply.github.com>
2025-06-20 17:07:26 +0700

    update

--- server/obj/Debug/net8.0/CRUDTodoApp.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/CRUDTodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUDTodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f88f11c094ddbc82301abad3c4b3f7fe21a39e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+564f5615c53664d9998f8c7bab2e99bdb01a7f40")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUDTodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUDTodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
